58591db5d04e54c425db983cea7627bd51107f7d
Schuy <schuylertk@gmail.com>
2024-06-04 15:43:17 -0600

    Can open inkscape within tex files

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -3,6 +3,35 @@ vim.opt_local.tabstop = 2
 vim.opt_local.spell = true
 vim.opt_local.conceallevel = 1
 
+local function file_exists(filename)
+	local file = io.open(filename, "r+")
+	if file ~= nil then
+		io.close(file)
+		return true
+	else
+		return false
+	end
+end
+
+function INKSCAPE()
+	local filename = vim.fn.getreg('f')
+	
+	filename = filename:gsub("%s+", "-")
+
+	local dir = vim.fn.getcwd()
+	local path = dir .. "/figures/" .. filename .. ".svg"
+
+	if file_exists(path) then
+		os.execute("inkscape " .. path)
+		return
+	end
+
+	local options = '"file-new:; export-filename:' .. path .. '; export-do"'
+	local cmd = "inkscape --actions=" .. options
+	os.execute(cmd)
+	os.execute("inkscape " .. path)
+end
+
 local status, which_key = pcall(require, "which-key")
 if status then
 	local opts = {
@@ -25,6 +54,9 @@ if status then
         s = { "<cmd>VimtexStop<cr>", "Stop Compilation" },
         e = { "<cmd>VimtexErrors<cr>", "Show Errors" },
         l = { "<cmd>VimtexClean<cr>", "Clean Auxiliary Files" },
+		f = { "<cmd>let @f = getline('.') | lua INKSCAPE() <cr>",
+			  "Open filename in inkscape."
+		  },
 	}
 
 	which_key.register(mappings, opts)
